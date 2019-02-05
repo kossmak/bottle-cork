c8b2b165911df7b231b33d3d3f00da83d2c2166d
kmakarov <konstantin.makarov@ramax.ru>
2019-02-05 19:24:54 +0300

    + ws.py samples and stubs:   + /bottle/bad_soap - response with improper xml scheme inside   + /bottle/crt/reserve - stub for aflcab service and   /sb/.../upcl4crt/v1   + /bottle/svc/error_rs - stub with sb json error filled structure   + /bottle/static/<filepath:path> - static files hosting   + /bottle/upload - sample how to get incoming file uploading

---
+++ ws.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import json
+import os
+import socket
+from pprint import pformat
+
+from bottle import route, template, run, request, response, post, static_file
+
+BOTTLE_STATIC_DIR = '/home/kossmak/proj/bottle-cork/static'
+# TIMEOUT = 62
+TIMEOUT = 60
+
+# socket.setdefaulttimeout(TIMEOUT)
+
+
+@route('/bottle/bad_soap', method='ANY')
+def bad_soap():
+    # response.headers['Content-Type'] = 'xml/application'
+    response.content_type = 'xml/application'
+    return u'''\
+<?xml version="1.0" encoding="UTF-8"?>
+<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+  <doc>
+    <tag code="RUB">111</tag>
+    <tag code="RUB">222</tag>
+  </doc>
+</soap:Envelope>
+'''
+
+
+@post('/bottle/crt/reserve')
+def index(**kw):
+    print "\n---------"
+    print "request: ", request.body.read()
+    print "cookie: ", pformat(request.cookies)
+    response.content_type = 'application/json'
+    result = {
+        "data": {
+            "reservationId": "FUPG891351"
+        },
+        "errors": [],
+        "isSuccess": True
+    }
+    return json.dumps(result)
+
+
+@post('/bottle/svc/error_rs')
+def index_error_rs(**kw):
+    print "\n---------"
+    print "request: ", request.body.read()
+    print "cookie: ", pformat(request.cookies)
+    response.content_type = 'application/json'
+    result = u'''\
+{
+    "isSuccess": false,
+    "errors": [
+        {
+            "message": "Не было найдено ваучеров по входящим параметрам",
+            "code": "1001000333",
+            "dbg": "SiebelNoCertificatesFound",
+            "value": null
+        }
+    ],
+    "data": null
+}
+    '''
+    return result
+    # return result.encode('utf-8')
+
+
+@route('/bottle/static/<filepath:path>')
+def server_static(filepath):
+    import time
+    time.sleep(1)
+    # time.sleep(TIMEOUT-50)
+    return static_file(filepath, root=BOTTLE_STATIC_DIR)
+
+
+@route('/bottle/upload', method='POST')
+def do_upload(**kw):
+    # category = request.forms.get('category')
+    saved_paths = []
+    if not request.files:
+        raise ValueError('Bad request format. No files.')
+    for param_name in request.files:
+        # upload = request.files.get('upload')
+        uploaded_file = request.files.get(param_name)
+        if not uploaded_file:
+            continue
+        name, ext = os.path.splitext(uploaded_file.filename)
+        # if ext not in ('.png', '.jpg', '.jpeg'):
+        #     return "File extension not allowed."
+
+        # save_path = "/tmp/{category}".format(category=category)
+        # save_path = '/tmp/{}{}'.format(name, ext)
+        save_path = os.path.join(os.path.dirname(__file__), 'static', 'incoming')
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+        # file_path = os.path.join(save_path, upload.filename)
+        file_path = os.path.join(save_path, uploaded_file.raw_filename)
+
+        if os.path.isfile(file_path):
+            # raise ReferenceError('File {} already uploaded'.format(file_path))
+            print '{} exists, removing...'.format(file_path)
+            os.unlink(file_path)
+            print '{} removed'.format(file_path)
+
+        uploaded_file.save(file_path)
+        # or for old bottle's versions:
+        # with open(file_path, 'wb') as open_file:
+        #     open_file.write(upload.file.read())
+        saved_paths.append(file_path)
+
+    return "Files successfully saved to \n'{0}'\n".format(pformat(saved_paths))
+
+
+# run(host='localhost', port=8880)
+run(host='0.0.0.0', port=8880)
+
